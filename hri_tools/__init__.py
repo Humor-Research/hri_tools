8cb66a5b9b9cb831c89ff48589cb3a3013ff0307
alexanderbaranof <alexanderbaranof@gmail.com>
2023-01-25 19:19:52 +0300

    feat: added train-valid-test split

--- hri_tools/__init__.py
+++ hri_tools/__init__.py
@@ -2,3 +2,5 @@ from .dataset_loader import HumorDataset
 from .utils import SUPPORTED_DATASETS
 from .utils import downlaod
 from .utils import calc_divergence
+from .utils import calc_divergence_between_target
+from .utils import calc_vocab_for_labels
